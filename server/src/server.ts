83ee96576418eaa12487cedec64ddf1386c6c64b
meliande <rodrigomeliande@msn.com>
2022-11-02 12:08:59 -0300

    first

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function boostrap() {
+    const fastify = Fastify({
+        logger: true,
+
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+
+    fastify.get('/polls/count', async () => {
+
+        const count = await prisma.poll.count()
+
+        return { count }
+    })
+
+    await fastify.listen({port: 3333 , host: '0.0.0.0'})
+}
+
+boostrap()
